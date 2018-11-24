49c49b5a4c016e6923b0faf7ba7f8aedeb29f3f4
bdodia <bdodia@users.noreply.github.com>
2018-11-24 22:24:57 +0000

    added data

---
+++ src/main/kotlin/Continent.kt
@@ -0,0 +1,11 @@
+package com.edgehoop.java8
+
+enum class Continent {
+    AFRICA,
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    SOUTH_AMERICA,
+    ANTARCTICA,
+    AUSTRALIA
+}
---
+++ src/main/kotlin/Country.kt
@@ -0,0 +1,8 @@
+package com.edgehoop.java8
+
+data class Country (
+    val country_iso_code: String,
+    val name: String,
+    val continent: Continent,
+    val population: Double
+)
